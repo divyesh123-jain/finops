f380d77006307695818cac5e641a12787f5b074a
Divyesh Jain <divyeshjain318@gmail.com>
2023-10-08 17:55:36 +0530

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,41 +1,24 @@
-import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+
 import { CiMail } from "react-icons/ci";
 import { AiOutlineBell } from "react-icons/ai";
-import { GiSuspicious } from "react-icons/gi";
-import { AiOutlineDown } from "react-icons/ai";
+import { User } from "@nextui-org/react";
 
 const Header: React.FC = () => {
   return (
     <>
-      <div className="hidden absolute top-0 right-0 md:flex justify-between w-[81%] md:rounded-tr-[40px] text-black h-[10vh] p-4 bg-white">
-        {/* <div className="inline-block"> LOGO </div> */}
-
-        {/* <div className="flex justify-between"> */}
-        <div className="w-96 bg-gray-100 h-8 flex justify-start items-center rounded-md p-2 gap-1">
-          <AiOutlineSearch /> Search
-        </div>
-
-        <div className="flex justify-between items-center gap-4">
-          <div>
+      <div className="hidden absolute top-0 right-0 md:flex justify-end w-[100%] md:rounded-tr-[40px] text-black h-[10vh] inlin-block p-4 bg-white rounded-tl-[40px]">
+        <div className="flex justify-between items-center gap-4">       
             <CiMail className="text-2xl" />
-          </div>
-          <div>
             <AiOutlineBell className="text-2xl" />
-          </div>
-          <div>|</div>
-          <div className="flex gap-2 items-center justify-start">
-            <div>
-              <GiSuspicious className="text-2xl" />
-            </div>
-            <div>Name Here</div>
-            <div>
-              <AiOutlineDown />
-            </div>
-          </div>
+            <span>|</span>
+            <User
+                name 
+                avatarProps={{
+                src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+            }}    
+            />
         </div>
-        {/* </div> */}
-      </div>
+     </div>
     </>
   );
 };
--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -7,14 +7,13 @@ const SideBar: React.FC = () => {
   return (
     <>
       <div className="bg-white hidden flex-[1] w-[19vw]  md:h-[100vh] md:flex item-center md:items-start md:flex-col gap-4 overflow-hidden rounded-l-[40px] border-r-2 z-10 border-slate-200">
-        <div className="p-4 flex  items-center"> {/*<Header />*/} </div>
+        <div className="p-2 flex  items-center"> {/*<Header />*/} </div>
 
-        <div className="relative md:w-full md:mt-4 flex flex-col gap-4 text-2xl  text-black p-4">
-          <div className="text-[1.1rem] flex items-center justify-start gap-2">
-            Dashboard
-          </div>
-          <div className="w-[95%] h-[100px] bg-gray-900 rounded-xl flex justify-evenly items-center p-2 text-white">
-            <div className="flex flex-col justify-evenly items-center">
+        <div className="relative md:w-full  flex flex-col gap-4   text-black p-4">
+          <span className="text-center text-3xl font-bold">FinOps</span>
+          
+          <div className="w-[95%] h-[100px] bg-gray-900 rounded-xl flex justify-evenly mt-4 items-center p-4 text-white">
+            <div className="flex flex-col mt-4justify-evenly items-center">
               <div className="text-sm">Total Investment</div>
               <div>$5380,90</div>
             </div>
